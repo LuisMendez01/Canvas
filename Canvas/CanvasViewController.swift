c6ada52d6358b53ea27b3f9fba0791c1fdcf7625
Luis Mendez <16315708+LuisMendez01@users.noreply.github.com>
2018-11-10 21:53:20 -0500

    Tray hides and shows on panning

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -2,29 +2,67 @@
 //  CanvasViewController.swift
 //  Canvas
 //
-//  Created by Silvia L Mendez on 11/10/18.
+//  Created by Luis Mendez on 11/10/18.
 //  Copyright © 2018 Luis Mendez. All rights reserved.
 //
 
 import UIKit
 
 class CanvasViewController: UIViewController {
+    
+    @IBOutlet weak var trayView: UIView!
+    
+    //for position
+    var trayOriginalCenter: CGPoint!
+    
+    //for speed/velocity
+    var trayDownOffset: CGFloat! = 0//The trayDownOffset will dictate how much the tray moves down.
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // The trayDownOffset will dictate how much the tray moves down
+        trayDownOffset = 270//270 worked for my tray, but you will likely have to adjust this value to accommodate the specific size of your tray.
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
+        
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        let location = sender.location(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        //When the gesture begins (.began), store the tray's center into the trayOriginalCenter variable:
+        if sender.state == .began {
+            print("Gesture began")
+            //store the tray's center into the trayOriginalCenter variable. This way each time the tray is panned, we will have a reference to where it was before the panning started.
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            // change the trayView.center by the translation. Note: we ignore the x translation because we only want the tray to move up and down:
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            
+        } else if sender.state == .ended {//Since we are focusing on the user's last gesture movement,
+            print("Gesture ended")
+            
+            //If the y component of the velocity is a positive value, the user is panning down
+            if velocity.y > 0 {
+                print("Moving down")
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                }, completion: nil)
+            } else {//If the y component is negative, the user is panning up.
+                print("Moving up")
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                }, completion: nil)
+            }
+        }
     }
-    */
-
 }
