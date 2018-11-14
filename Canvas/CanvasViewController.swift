3efadd3c6ba4e1f91564426b48d74096347024d1
Luis Mendez <16315708+LuisMendez01@users.noreply.github.com>
2018-11-14 12:11:59 -0500

    faces can be copied and dragged from tray to main view

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -15,6 +15,9 @@ class CanvasViewController: UIViewController {
     //for position
     var trayOriginalCenter: CGPoint!
     
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
     //for speed/velocity
     var trayDownOffset: CGFloat! = 0//The trayDownOffset will dictate how much the tray moves down.
     var trayUp: CGPoint!
@@ -33,9 +36,9 @@ class CanvasViewController: UIViewController {
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
         
         let translation = sender.translation(in: view)
-        let location = sender.location(in: view)
+        //let location = sender.location(in: view)
         let velocity = sender.velocity(in: view)
-        
+
         //When the gesture begins (.began), store the tray's center into the trayOriginalCenter variable:
         if sender.state == .began {
             print("Gesture began")
@@ -45,10 +48,10 @@ class CanvasViewController: UIViewController {
             print("Gesture is changing")
             // change the trayView.center by the translation. Note: we ignore the x translation because we only want the tray to move up and down:
             trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
-            
+
         } else if sender.state == .ended {//Since we are focusing on the user's last gesture movement,
             print("Gesture ended")
-            
+
             //If the y component of the velocity is a positive value, the user is panning down
             if velocity.y > 0 {
                 print("Moving down")
@@ -63,6 +66,70 @@ class CanvasViewController: UIViewController {
                                 self.trayView.center = self.trayUp
                 }, completion: nil)
             }
+        }
+    }
+    
+    //drag and create new faces from canvas
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
+        print("translation: \(translation)")
+        
+        if sender.state == .began {
+            print("didPanFace Gesture began")
+            
+            let imageView = sender.view as! UIImageView//imageView now refers to the face that you panned on.
+            
+            newlyCreatedFace = UIImageView(image: imageView.image) //Create a new image view that has the same image as the one you're currently panning.
+            
+            // The didMoveFace: will move the little faces
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didMoveFace(_:)))
+            
+            // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
+            
+            view.addSubview(newlyCreatedFace)// Add the new face to the main view.
+            
+            newlyCreatedFace.center = imageView.center //Initialize the position of the new face.
+            
+            print("newlyCreatedFace.center: \(newlyCreatedFace.center)")
+            print("trayView.frame.origin.y: \(trayView.frame.origin)")
+            
+            //since face y is 114.0 inside tray then we want to know what is 114.0 for the main view
+            //outside the tray, so tray 567+114= the position of face according to main view.
+            newlyCreatedFace.center.y = trayView.frame.origin.y //Since the original face is in the tray, but the new face is in the main view, you have to offset the coordinates.
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            
+            UIView.animate(withDuration: 1,
+                           animations: {
+                            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 0.6, y: 0.6)
+            },
+                           completion: { _ in
+                            UIView.animate(withDuration: 0.6) {
+                                self.newlyCreatedFace.transform = CGAffineTransform.identity
+                            }
+            })
+            
+        } else if sender.state == .changed {
+            print("didPanFace Gesture is changing")
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        }
+    }
+    
+    //allows to move smiley faces that were created from dragging from canvas
+    @objc func didMoveFace(_ sender: UIPanGestureRecognizer) {
+
+        let translation = sender.translation(in: view)
+
+        if sender.state == .began {
+            print("didMoveFace Gesture began")
+            newlyCreatedFace = sender.view as? UIImageView // to get the face that we panned on.
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center // so we can offset by translation later.
+        }else if sender.state == .changed {
+            print("didMoveFace Gesture is changing")
+            //position will where face was to where it was translated to
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
         }
     }
 }
